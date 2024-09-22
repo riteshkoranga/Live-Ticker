7cd929dbfec0d64e074e7b30ff30ecbdd4d1a128
riteshkoranga <riteshkoranga5@gmail.com>
2024-09-22 20:17:56 +0530

    added media queris to make it mobile compatible and reduce text size

--- src/components/Ticker_fetch.jsx
+++ src/components/Ticker_fetch.jsx
@@ -18,9 +18,8 @@ const StockTicker = () => {
   useEffect(() => {
     const fetchStockData = async () => {
       try {
-        const response = await axios.get(
-          "https://livetickerbackend-production.up.railway.app/"
-        );
+        const response = await axios.get("http://localhost:4000/");
+        //"https://livetickerbackend-production.up.railway.app/"
         const data = response.data;
         console.log(data);
 
@@ -66,11 +65,11 @@ const StockTicker = () => {
   return (
     <div className="p-3">
       <div className="flex flex-col md:flex-row md:justify-between space-y-4 md:space-y-1">
-        <div className="text-3xl md:text-5xl text-cyan-600 font-raleway">
+        <div className="text-4xl font-medium text-center md:text-5xl text-cyan-600 font-raleway">
           Live Ticker
         </div>
 
-        <div className="flex md:items-center space-x-7 ml-5">
+        <div className="flex md:items-center items-center justify-center space-x-3 ml-5">
           {/* Custom Dropdown for Currency */}
           <div className="mt-5">
             <CustomDropdown
@@ -95,9 +94,9 @@ const StockTicker = () => {
           </button>
         </div>
 
-        <div className="flex space-x-3">
+        <div className="flex space-x-3 justify-end">
           {/* Connect Button */}
-          <button className="flex bg-cyan-600 p-2 rounded-lg text-white font-raleway mb-7 text-sm">
+          <button className="flex bg-cyan-600 p-2 rounded-lg text-white font-raleway mb-7 text-sm ">
             Connect Telegram
           </button>
 
@@ -110,8 +109,8 @@ const StockTicker = () => {
               onChange={handleConnectToggle}
             />
             <div
-              className={`w-12 h-6 flex items-center bg-gray-300 rounded-full p-1 cursor-pointer ${
-                isConnected ? "bg-cyan-700" : "bg-gray-600"
+              className={`w-12 h-6 flex items-center rounded-full p-1 cursor-pointer ${
+                isConnected ? "bg-cyan-600" : "bg-gray-600"
               }`}
             >
               <div
@@ -124,10 +123,10 @@ const StockTicker = () => {
         </div>
       </div>
 
-      <div className="text-center p-2 mt-3 sm:ml-0 md:ml-10 w-full text-gray-500 font-mono text-xs md:text-sm">
+      <div className="text-center p-2 mt-3 sm:ml-0 md:ml-10 w-full text-gray-500 font-mono text-base md:text-sm">
         Best Price To Trade
       </div>
-      <div className="text-center px-2 sm:ml-0 md:ml-10 w-full sm:text-4xl md:text-5xl text-white font-thin">
+      <div className="text-center px-2 text-5xl sm:ml-0 md:ml-10 w-full sm:text-4xl md:text-5xl text-white font-thin">
         <h1>
           {btcInr ? (
             <p>
@@ -137,33 +136,35 @@ const StockTicker = () => {
               }).format(btcInr)}
             </p>
           ) : (
-            <p className="font-thin text-lg text-white">Loading...</p>
+            <p className="font-thin text-white text-base">Loading...</p>
           )}
         </h1>
       </div>
-      <h4 className="text-center p-4 sm:ml-0 md:ml-10 w-full sm:text-xs md:text-sm text-gray-400 font-thin">
+      <h4 className="text-center p-4 text-xs sm:ml-0 md:ml-10 w-full sm:text-xs md:text-sm text-gray-400 font-thin">
         Average BTC/INR price new including commission
       </h4>
 
       <div className="flex justify-center min-h-screen p-4">
         <ul className="w-full max-w-7xl bg-inherit shadow-lg rounded-lg">
-          <li className="bg-inherit text-gray-500 p-4 rounded-t-lg flex flex-items font-raleway">
-            <span className="w-1/12 text-center text-lg md:text-xl">#</span>
-            <span className="w-3/12 text-lg md:text-xl text-center">Name</span>
-            <span className="w-2/5 text-lg md:text-xl text-center">
+          <li className="bg-inherit text-gray-500 p-4 rounded-t-lg flex flex-items font-raleway space-x-1">
+            <span className="w-1/12 text-center text-base md:text-xl">#</span>
+            <span className="w-3/12 text-base md:text-xl text-center">
+              Name
+            </span>
+            <span className="w-2/5 text-base md:text-xl text-center">
               Last Traded Price
             </span>
-            <span className="w-3/6 text-lg md:text-xl text-center">
+            <span className="w-3/6 text-base md:text-xl text-center">
               High / Low Price
             </span>
-            <span className="w-3/12 text-lg md:text-xl text-center">
+            <span className="w-3/12 text-base md:text-xl text-center">
               Difference
             </span>
           </li>
           {tickers.map((ticker, index) => (
             <li
               key={index}
-              className="bg-gray-700 hover:bg-gray-800 rounded-lg my-2 p-4 flex items-center text-white text-lg md:text-xl group"
+              className="bg-gray-700 hover:bg-gray-800 rounded-lg my-2 p-4 flex items-center text-white text-base md:text-xl group"
             >
               <span className="w-1/12 text-center font-thin group-hover:font-light">
                 {index + 1}
